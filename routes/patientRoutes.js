204911e986e1ba2ee0bee81a4eff28585422b142
Henil Tandel <heniltandel0905@gmail.com>
2025-02-23 14:19:06 +0530

    updated patientroutes

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const { registerPatient, loginPatient, searchDoctors,searchDoctorsBySpecialzation, bookAppointment, getAppointments } = require("../controllers/patientController");
 const authenticate = require("../middleware/authMiddleware");
-
 const router = express.Router();
 
 router.post("/register", registerPatient);
@@ -10,5 +9,6 @@ router.get("/search-doctors", searchDoctors);
 router.get("/specialization",searchDoctorsBySpecialzation);
 router.post("/book-appointment", authenticate, bookAppointment);
 router.get("/appointments/:patientId", authenticate, getAppointments);
+
 
 module.exports = router;
