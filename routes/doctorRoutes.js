60dbb3218eceeba48a02a30f4ab82db0d3ece386
Henil Tandel <heniltandel0905@gmail.com>
2025-02-25 20:26:37 +0530

    forgot and reset password routes

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerDoctor, loginDoctor, addAvailability, searchDoctors } = require("../controllers/doctorController");
+const { registerDoctor, loginDoctor, addAvailability,forgotPasswordDoctor,resetPasswordDoctor } = require("../controllers/doctorController");
 const authenticate = require("../middleware/authMiddleware");
 
 const router = express.Router();
@@ -7,5 +7,10 @@ const router = express.Router();
 router.post("/register", registerDoctor);
 router.post("/login", loginDoctor);
 router.post("/add-availability", authenticate, addAvailability);
+
+
+router.post("/forgot-password", forgotPasswordDoctor);
+router.post("/reset-password/:token", resetPasswordDoctor);
+
 
 module.exports = router;
