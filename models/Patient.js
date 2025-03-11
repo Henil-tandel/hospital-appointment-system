4866a11391188a46cbb4ad557288dedb23f06fdf
Henil Tandel <heniltandel0905@gmail.com>
2025-03-11 11:33:38 +0530

    added field  review for rating doctor to the patient Schema

--- models/Patient.js
+++ models/Patient.js
@@ -6,6 +6,12 @@ const patientSchema = new mongoose.Schema({
     password: { type: String, required: true },
     age: { type: Number, required: true, min: 1 },
     gender: { type: String, enum: ["Male", "Female", "Other"], required: true },
+    reviews: [{
+        patientId: { type: mongoose.Schema.Types.ObjectId, ref: "Patient" }, 
+        doctorId: { type: mongoose.Schema.Types.ObjectId, ref: "Doctor" },
+        rating: { type: Number, required: true, min: 0, max: 5 },
+        comment: { type: String }
+    }],
     createdAt: { type: Date, default: Date.now }
 });
 
