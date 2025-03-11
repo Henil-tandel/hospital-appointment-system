a6335e93b0684be9e4ca289a4eb22c8f75e416a2
Henil Tandel <heniltandel0905@gmail.com>
2025-03-11 11:30:03 +0530

    added view and update profile routes for doctor

--- routes/doctorRoutes.js
+++ routes/doctorRoutes.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const { registerDoctor, loginDoctor, addAvailability,getDoctorAppointments,updateAppointment,cancelAppointment,forgotPasswordDoctor,resetPasswordDoctor } = require("../controllers/doctorController");
+const { 
+    registerDoctor, 
+    loginDoctor,
+    viewProfile,
+    updateDetails,
+    addAvailability,
+    getDoctorAppointments,
+    updateAppointment,
+    cancelAppointment,
+    forgotPasswordDoctor,
+    resetPasswordDoctor 
+} = require("../controllers/doctorController");
 const authenticate = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.post("/register", registerDoctor);
 router.post("/login", loginDoctor);
+router.get('/profile/:doctorId',authenticate,viewProfile);
+router.patch('/update-details',authenticate,updateDetails);
 router.post("/add-availability", authenticate, addAvailability);
 router.post('/appointments/:doctorId',authenticate,getDoctorAppointments);
 router.patch("/update-appointment/:appointmentId", updateAppointment);
-router.delete('/delete-appointment/:appointmentId',cancelAppointment)
+router.delete('/delete-appointment/:appointmentId',cancelAppointment);
+
 router.post("/forgot-password", forgotPasswordDoctor);
 router.post("/reset-password/:token", resetPasswordDoctor);
 
