7d5a7f19d1956600506bc46d8ee00bb602192b3d
Henil Tandel <heniltandel0905@gmail.com>
2025-03-11 11:31:11 +0530

    added view and update profile routes and rate doctor and getDoctorsByRating , searchDoctorsByLocation routes

--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -1,17 +1,35 @@
 const express = require("express");
-const { registerPatient, loginPatient, searchDoctors,searchDoctorsBySpecialization, bookAppointment, getAppointments,cancelAppointment,forgotPasswordPatient,resetPasswordPatient } 
-= require("../controllers/patientController");
+const { 
+    registerPatient, 
+    loginPatient, 
+    viewProfile,
+    updateDetails,
+    searchDoctors,
+    searchDoctorsBySpecialization,
+    searchDoctorsByLocation,
+    rateDoctor,
+    getDoctorsByRating,
+    bookAppointment, 
+    getAppointments,
+    cancelAppointment,
+    forgotPasswordPatient,
+    resetPasswordPatient 
+} = require("../controllers/patientController");
 const authenticate = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", registerPatient);
 router.post("/login", loginPatient);
+router.get('/profile/:patientId',authenticate,viewProfile);
+router.patch('/update-details',authenticate,updateDetails);
 router.get("/search-doctors", searchDoctors);
 router.get("/specialization", searchDoctorsBySpecialization);
+router.post('/rate-doctor',authenticate,rateDoctor);
+router.get('/rating',getDoctorsByRating);
+router.get('/location',searchDoctorsByLocation);
 router.post("/book-appointment", authenticate, bookAppointment);
 router.get("/appointments/:patientId", authenticate, getAppointments);
 router.delete("/delete-appointment/:appointmentId", authenticate, cancelAppointment);
-
 router.post("/forgot-password", forgotPasswordPatient);
 router.post("/reset-password/:token", resetPasswordPatient);
 
